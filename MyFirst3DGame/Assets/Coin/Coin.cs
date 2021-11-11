9fa4a0e5db69535e4e68a84c042db11cf3429ec3
Adrián Vázquez <avazmir1337@gmail.com>
2021-11-11 14:23:11 +0100

    First Game

---
+++ MyFirst3DGame/Assets/Coin/Coin.cs
@@ -0,0 +1,85 @@
+﻿/*
+ *  NAMESPACE
+ */
+using System.Collections;
+using UnityEngine;
+
+/*
+ * CLASS
+ */ 
+public class Coin : MonoBehaviour
+{
+
+    /*
+     *  PROPIEDADES Y MÉTODOS
+     */
+
+    public static int coinsCount = 0; // Una variable static se comparte entre todas las instancias, en este caso solo va a haber un contador para todas las monedas.
+    
+    private float initialYpos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Guardamos la posición inicial del gameObject en la coordenada Y
+        initialYpos = transform.position.y;
+        Coin.coinsCount++;
+        Debug.Log("El juego ha comenzado y hay " + Coin.coinsCount + " monedas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotar();
+        Rebotar(initialYpos); // Pasamos la posición inicial guardada al método.
+    }
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if(otherCollider.CompareTag("Player"))
+        {
+            // Decrecemos el contador
+            Coin.coinsCount--;
+
+            Debug.Log("Hemos recogido una moneda y quedan " + Coin.coinsCount + " monedas");
+
+            //Ganamos
+            if (Coin.coinsCount == 0)
+            {
+                Debug.Log("¡Has ganado!");
+
+                GameObject gameManager = GameObject.Find("GameManager");
+                Destroy(gameManager); //Paramos el tiempo destruyendolo.
+
+                // Activamos los fuegos artificiales
+                GameObject[] fireworksSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+
+                foreach( GameObject fireworks in fireworksSystem)
+                {
+                    fireworks.GetComponent<ParticleSystem>().Play();
+                }
+            }
+
+            Destroy(gameObject);
+        }
+        
+
+    }
+
+    private void Rotar()
+    {
+        transform.Rotate(0, 0, 360 * Time.deltaTime * .25f);
+    }
+
+    private void Rebotar(float Ypos)
+    {
+        /*
+         * PingPong es un metodo que devuelve un rango entre 0 y el limite dado y de este a 0.
+         * En nuestro caso nos interesa que vaya desde la posición inicial del GameObject (Ypos en este caso) hasta el limite dado
+         * y de este limite a nuestra posición inicial Ypos.
+         */
+        float y = Mathf.PingPong(Time.time * .25f, .5f) + Ypos;
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+    }
+}
---
+++ MyFirst3DGame/Assets/Manager/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    // Tiempo máximo 60 segundos
+    public float maxTime = 60f;
+
+    private float countdown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // El deltaTime es el tiempo en segundos que ha pasado desde que se renderizó en la pantalla el último frame anterior.
+        countdown -= Time.deltaTime;
+
+        Debug.Log("Tiempo :" + countdown);
+
+        //El juagor pierde
+        if (countdown <= 0f)
+        {
+            Debug.Log("Te has quedado sin tiempo... ¡FIN DEL JUEGO!");
+
+            //Reiniciamos la partida
+            Coin.coinsCount = 0;
+            SceneManager.LoadScene("MainScene");
+
+        }
+    }
+}
